a8adbd98fd8fa7f4a5b23b4fa60c34837f1125cb
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2020-12-13 01:36:48 +0300

    adaptive for first page burger menu by jquery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-$(function(){
+$(function () {
 
     $('.slider').slick({
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
-        dots:true
+        dots: true
     });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').toggleClass('menu__list--active')
+
+    });
+
+
 
 });
